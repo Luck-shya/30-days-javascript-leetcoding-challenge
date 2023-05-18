9140f8f6745c0006ac89b8acff77cf43d5c8c877
Laksh <64250608+Luck-shya@users.noreply.github.com>
2023-05-18 22:43:36 +0530

    Create Day8.js

---
+++ Day8.js
@@ -0,0 +1,18 @@
+
+var once = function(fn) {
+
+    let ans = true;
+
+    return function(...args){
+
+        if (ans) {
+
+            ans = false;
+
+            return fn(...args);
+
+        }
+
+    }
+
+};
